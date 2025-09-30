80ef24218536d72a3811c14b65d598eab789470f
Caleb Kopp <caleb.m.kopp@outlook.com>
2025-09-30 10:39:11 -0500

    Update vsc_install_list.sh

--- lists/vsc_install_list.sh
+++ lists/vsc_install_list.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+code --install-extension 4ops.terraform
 code --install-extension aaron-bond.better-comments
 code --install-extension albert.tabout
 code --install-extension angular.ng-template
@@ -22,7 +23,6 @@ code --install-extension github.vscode-github-actions
 code --install-extension github.vscode-pull-request-github
 code --install-extension golang.go
 code --install-extension grapecity.gc-excelviewer
-code --install-extension hashicorp.terraform
 code --install-extension henoc.svgeditor
 code --install-extension hungvo.htext
 code --install-extension janisdd.vscode-edit-csv
@@ -30,15 +30,13 @@ code --install-extension jeff-hykin.polacode-2019
 code --install-extension jock.svg
 code --install-extension johnpapa.angular2
 code --install-extension laurencebahiirwa.deno-std-lib-snippets
-code --install-extension manuth.markdown-converter
-code --install-extension marp-team.marp-vscode
 code --install-extension mattlott.copy-github-url
 code --install-extension mechatroner.rainbow-csv
 code --install-extension mikestead.dotenv
 code --install-extension mindaro-dev.file-downloader
 code --install-extension mohsen1.prettify-json
 code --install-extension ms-azuretools.vscode-azureresourcegroups
-code --install-extension ms-azuretools.vscode-azureterraform
+code --install-extension ms-azuretools.vscode-containers
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 code --install-extension ms-mssql.data-workspace-vscode
@@ -58,13 +56,12 @@ code --install-extension ms-toolsai.vscode-jupyter-slideshow
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-vscode.azure-account
 code --install-extension ms-vscode.live-server
 code --install-extension ms-vscode.makefile-tools
 code --install-extension ms-vscode.remote-explorer
 code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension nrwl.angular-console
 code --install-extension pkief.material-icon-theme
+code --install-extension pollywoggames.pico8-ls
 code --install-extension pranaygp.vscode-css-peek
 code --install-extension pucelle.vscode-css-navigation
 code --install-extension quicktype.quicktype
@@ -77,7 +74,6 @@ code --install-extension rogalmic.bash-debug
 code --install-extension rvest.vs-code-prettier-eslint
 code --install-extension scala-lang.scala
 code --install-extension shakram02.bash-beautify
-code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension steoates.autoimport
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension timonwong.shellcheck
@@ -86,6 +82,7 @@ code --install-extension unifiedjs.vscode-mdx
 code --install-extension visualstudioexptteam.intellicode-api-usage-examples
 code --install-extension visualstudioexptteam.vscodeintellicode
 code --install-extension vitaliymaz.vscode-svg-previewer
+code --install-extension vscjava.migrate-java-to-azure
 code --install-extension vscjava.vscode-gradle
 code --install-extension vscjava.vscode-java-debug
 code --install-extension vscjava.vscode-java-dependency
@@ -93,9 +90,7 @@ code --install-extension vscjava.vscode-java-pack
 code --install-extension vscjava.vscode-java-test
 code --install-extension vscjava.vscode-lombok
 code --install-extension vscjava.vscode-maven
-code --install-extension vscode-icons-team.vscode-icons
 code --install-extension william-voyek.vscode-nginx
-code --install-extension xyc.vscode-mdx-preview
-code --install-extension yzane.markdown-pdf
+code --install-extension yoavbls.pretty-ts-errors
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension zhuangtongfa.material-theme
